81127be7487b1488004ecc3bd66e889accb36892
lei <oizhaolei@gmail.com>
2022-08-22 16:28:12 +0900

    shutdown_signal

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use sea_orm::Database;
 use std::str::FromStr;
 use std::{env, net::SocketAddr};
 use tera::Tera;
+use tokio::signal;
 use tower::ServiceBuilder;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
@@ -45,7 +46,11 @@ async fn main() -> anyhow::Result<()> {
             .layer(Extension(conn))
             .layer(Extension(templates)),
     );
-    Server::bind(&addr).serve(app.into_make_service()).await?;
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
 
     Ok(())
 }
@@ -72,6 +77,31 @@ fn app() -> Router {
                 )
             }),
         )
+}
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
 #[cfg(test)]
 mod tests {
